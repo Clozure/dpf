838d33261422d31705235052ccccd46b875814b1
R. Matthew Emerson <rme@acm.org>
2012-02-11 00:57:47 +0000

    Add fullscreen windows.  This only works on Lion (but the binary should still work on Snow Leopard).  This causes us to lose the small title bar.  On the other hand, the Window menu now shows the slideshow windows.
    See ticket:20.  I still need to add an "Enter Full Screen"
    menu item.

--- dpf.lisp
+++ dpf.lisp
@@ -458,8 +458,7 @@
      #&NSApplicationDidBecomeActiveNotification +null-ptr+)))
 
 
-;;; This is a panel so we can get the small title bar.
-(defclass slideshow-window (ns:ns-panel)
+(defclass slideshow-window (ns:ns-window)
   ()
   (:metaclass ns:+ns-object))
 
@@ -476,6 +475,12 @@
    (source :initform nil :accessor slideshow-source))
   (:metaclass ns:+ns-object))
 
+;;; #$NSAppKitVersionNumber10_6 isn't in the interface database
+(defconstant snow-leopard-appkit-version 1038d0)
+
+(defun lionp ()
+  (> (floor #$NSAppKitVersionNumber) snow-leopard-appkit-version))
+ 
 ;;; This could also be done in an #/initWithWindow: override
 (defmethod initialize-instance :after ((x slideshow-window-controller)
 				       &rest initargs)
@@ -485,6 +490,12 @@
 	  transition *slide-transition*
 	  order *slide-order*
 	  on-top-p *slide-on-top-p*)
+    (when (lionp)
+      ;; enable fullscreen
+      (objc:objc-message-send (#/window x)
+			      "setCollectionBehavior:"
+			      ;; NSWindowCollectinoBehaviorFullScreenPrimary
+			      #>NSUInteger (ash 1 7)))
     (when on-top-p
       (#/setLevel: (#/window x)
 		   (#_CGWindowLevelForKey #$kCGFloatingWindowLevelKey)))))
@@ -890,9 +901,10 @@
 	       (#/alloc (objc:@class "SlideshowWindow"))
 	       r
 	       (logior ;#$NSHUDWindowMask
-		       #$NSUtilityWindowMask
+		       ;#$NSUtilityWindowMask
 		       #$NSTitledWindowMask
 		       #$NSClosableWindowMask
+		       #$NSMiniaturizableWindowMask
 		       #$NSResizableWindowMask)
 	       #$NSBackingStoreBuffered
 	       #$NO))
@@ -902,7 +914,7 @@
       (#/release w)
       (#/setMovableByWindowBackground: w t)
       (#/setHidesOnDeactivate: w nil)
-      (#/setFloatingPanel: w nil)
+      ;;(#/setFloatingPanel: w nil)
       (#/setDelegate: w wc)
       (if (directory-pathname-p title)
 	(with-cfstring (s (native-translated-namestring title))
