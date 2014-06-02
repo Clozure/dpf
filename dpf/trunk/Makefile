4efc9aa963ec7260db0f23aee7f601da74a8f3b3
greg <greg@3d7b062c-1b4d-4122-a2b0-d4ba9f02c56a>
2014-06-02 22:38:44 +0000

    Fix failure of `ibtool` on Mavericks (by building with `make`, rather than within CCL).

---
+++ dpf/trunk/Makefile
@@ -0,0 +1,18 @@
+CONTENTS="build/Picture Window.app/Contents/"
+BINARY="$(CONTENTS)/MacOS/Picture Window"
+
+%.nib : en.lproj/%.xib
+	ibtool --errors --warnings --notices --output-format human-readable-text --compile $@ $<
+
+NIBS=MainMenu.nib dpf-preferences.nib help.nib
+
+build:  nibs 
+	mkdir -p $(CONTENTS)
+	cp $(NIBS) $(CONTENTS)
+	~/Documents/Clozure/ccl-clean/dx86cl64 --no-init --load build.lisp
+
+nibs: $(NIBS)
+	
+
+clean:
+	rm -f $(NIBS)
--- dpf/trunk/build.lisp
+++ dpf/trunk/build.lisp
@@ -21,27 +21,6 @@
 				 "help.html"
 				 "app.icns"))
 
-(defun compile-xib (xib &optional output-nib)
-  (unless output-nib
-    (setq output-nib (make-pathname :type "nib" :defaults xib)))
-  (with-output-to-string (s)
-    (let* ((nib-namestring (native-translated-namestring output-nib))
-           (xib-namestring (native-translated-namestring xib))
-           (p (run-program *ibtool-program*
-                           (list "--errors" "--warnings" "--notices"
-                                 "--output-format" "human-readable-text"
-                                 "--compile" nib-namestring
-                                 xib-namestring)
-                           :output s
-                           :error :output)))
-      (multiple-value-bind (status exit-code)
-          (external-process-status p)
-        (unless (and (eq :exited status)
-                     (zerop exit-code))
-          (error "Error compiling xib file ~s:~%~a" xib
-                 (or (ccl::describe-external-process-failure p "no ibtool?")
-                     (get-output-stream-string s))))))))
-
 (defun build-dpf (&optional (build-dir *build-dir*))
   (let* ((*build-dir* build-dir)
 	 (*bundle-dir* (merge-pathnames "Picture Window.app/" *build-dir*))
@@ -55,12 +34,6 @@
     (ensure-directories-exist *macos-dir*)
     (copy-file "Info.plist" (merge-pathnames "Info.plist" *contents-dir*)
 	       :if-exists :supersede)
-    (dolist (x (directory (merge-pathnames "en.lproj/*.xib")))
-      (let ((dest (make-pathname :name (pathname-name x)
-				 :type "nib"
-				 :defaults (merge-pathnames "en.lproj"
-							    *resources-dir*))))
-	(compile-xib x dest)))
     (dolist (f *resource-files*)
       (copy-file f (merge-pathnames f *resources-dir*)
 		 :if-exists :supersede))
